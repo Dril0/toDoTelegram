744f33bc232ece3c5e06be1e7619c91ac7f02690
Jeremias <jeremias.acurso@hotmail.com>
2024-06-12 09:26:24 -0300

    ruteamos urls de accounts/urls.py

--- toDoTelegram/urls.py
+++ toDoTelegram/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path(
         "accounts/", include("django.contrib.auth.urls")
     ),  # incluimos las urls por defecto de auth app --> login, logout, passwords and resets. con el template context de django nos permite cargar los templates con data correspondiente a las views. usamos user tags para acceder a los atributos, por ejemplo user.email.
+    path("accounts/", include("accounts.urls")),
     path("", include("pages.urls")),
 ]
