0a0e0d072906dc82b2cec92cca633ece33cddf11
Jeremias Acurso <jeremias.acurso@hotmail.com>
2024-06-17 13:15:07 -0300

    creamos la view task_list que importa el modelo Task con el usuario y renderiza el html task_list copn el formulario task

---
+++ toDo/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Task
+
+# Create your views here.
+
+
+@login_required
+def task_list(request):
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, "todo/task_list.html", {"tasks": tasks})
