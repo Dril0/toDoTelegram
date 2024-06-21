6c2fa087693cec183a63b692ea04806438e362e1
Jeremias Acurso <jeremias.acurso@hotmail.com>
2024-06-21 15:10:22 -0300

    podemos utilizar los nuevos campos edit reaso y due_date

--- toDo/forms.py
+++ toDo/forms.py
@@ -4,6 +4,11 @@ from .models import Task
 
 
 class TaskForm(forms.ModelForm):
+    due_date = forms.DateTimeField(
+        widget=forms.DateTimeInput(attrs={"type": "datetime-local"}), required=False
+    )
+    edit_reason = forms.CharField(widget=forms.Textarea, required=False)
+
     class Meta:
         model = Task
-        fields = ["title", "description"]
+        fields = ["title", "description", "due_date", "edit_reason"]
