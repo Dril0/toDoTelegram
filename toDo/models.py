bb684f7dce397be159314376a812af297fb04945
Jeremias Acurso <jeremias.acurso@hotmail.com>
2024-06-21 15:09:16 -0300

    agreagamos los campos para poermitir elegir en un calendareio cuando realizar la tarea, y un campo para los motivos del cambio si se edita un archivo, se crea el modelo taskedithistory para poder guardar ek historial de ediciones de una tarea

--- toDo/models.py
+++ toDo/models.py
@@ -8,7 +8,29 @@ class Task(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    due_date = models.DateTimeField(
+        null=True, blank=True
+    )  # nos permite asegurar que ciertas tareas se realizen en un momento espoecifico.
+    edit_reason = models.TextField(
+        null=True, blank=True
+    )  # nos va a devolver una razon por la cual se edita la tarea.
     # Otros campos como fecha, estado de completado, etc.
 
     def __str__(self):
         return self.title
+
+
+class TaskEditHistory(models.Model):
+    """guarda el historial de ediciones, cuando una tarea es editad se puede crear una instancia de TaskEditHistory para registrar quien hizo la edición, cuando  se hizo y el motivo del cambio"""
+
+    task = models.ForeignKey(
+        Task, on_delete=models.CASCADE, related_name="edit_history"
+    )
+    edited_at = models.DateTimeField(auto_now_add=True)
+    edit_reason = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Edit by {self.user.username} on {self.edited_at}"
